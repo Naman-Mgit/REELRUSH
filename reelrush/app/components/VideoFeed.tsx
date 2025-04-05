1cc2432298cb33de18ccaff76e80340cbb642ae8
Naman <naman.apk.com@gmail.com>
2025-04-05 14:59:33 +0530

    More Ui fixes

--- reelrush/app/components/VideoFeed.tsx
+++ reelrush/app/components/VideoFeed.tsx
@@ -8,12 +8,14 @@ interface VideoFeedProps{
 const VideoFeed = ({videos}:VideoFeedProps) => {
   return (
     <>
-       {videos.map((video)=>(
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
+        {videos.map((video)=>(
           <VideoComponent key={video._id?.toString()} video={video}/>
-       ))}
-        {videos.length===0 &&
+        ))}
+       </div>
+       {videos.length===0 &&
            <div className="col-span-full text-center py-12">
-                 <p className="text-base-content/70">No videos found</p>
+                 <h1 className="text-base-content/70">No videos found</h1>
             </div>
         }
 
